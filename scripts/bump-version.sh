062580d6c49dfac0d9dbccc82f8be5f8cb4bda99
Gabriel Mateos <gmdcwork@gmail.com>
2020-05-14 10:08:08 +0200

    fix script

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -5,12 +5,12 @@ BRANCH=$(echo $1 | awk -F "[ ]+" '/feature(s)?\/(major|minor)\/.+/{print $NF}')
 
 if echo BRANCH | grep -q major; then
     # Bump major version
-    sh scripts/incr-version.sh --major
+    sh incr-version.sh --major
 elif echo BRANCH | grep -q minor; then
     # Bump minor version
-    sh scripts/incr-version.sh --minor
+    sh incr-version.sh --minor
 else
     # Bump patch version
-    sh scripts/incr-version.sh
+    sh incr-version.sh
 fi
 
